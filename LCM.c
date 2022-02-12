cd775de03b57d381134cafbd42cb33a7952271e4
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-02-12 14:09:03 +0530

    #include<stdio.h> int main () {     int a,b,i,max;     scanf("%d%d",&a,&b);     max=(a>b) ? a:b;     for(i=max;i<=a*b;i++)     if(i%a==0 && i%b==0)     {         break;     }     printf("%d",i);     return 0;
    }

---
+++ LCM.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b,i,max;
+    scanf("%d%d",&a,&b);
+    max=(a>b) ? a:b;
+    for(i=max;i<=a*b;i++)
+    if(i%a==0 && i%b==0)
+    {
+        break;
+    }
+    printf("%d",i);
+    return 0;
+    
+}
