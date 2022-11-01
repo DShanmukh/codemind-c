4eabd3b3e3f3fd7b281518bfa389640b38e04006
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-11-01 21:10:53 +0530

    #include<stdio.h> int main() { 	int n; 	scanf("%d",&n); 	int a[n],i,j,m; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{ 		for(j=i+1;j<n;++j) 		{ 			if(a[i]>a[j]) 			{ 				m=a[i]; 				a[i]=a[j]; 				a[j]=m; 			} 		} 	} 	for(i=0;i<n;i++) 	{ 		printf("%d ",a[i]); 	} }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n],i,j,m;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;++j)
+		{
+			if(a[i]>a[j])
+			{
+				m=a[i];
+				a[i]=a[j];
+				a[j]=m;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+}
