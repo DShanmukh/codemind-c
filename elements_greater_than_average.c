fd0b4cd6b2012101d9f98c0da921dbf3fc7af4b6
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-11-29 08:05:26 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,count=0,av=0,sum=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         sum=sum+a[i];         av=sum/n;     }     for(i=0;i<n;i++)     {         if(a[i]>av ||a[i]==av)         {            count++;         }     }     printf("%d",count);
    }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,count=0,av=0,sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+        av=sum/n;
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>av ||a[i]==av)
+        {
+           count++;
+        }
+    }
+    printf("%d",count);
+    
+}
