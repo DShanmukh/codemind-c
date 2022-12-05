465d8ba0ca325e8344a48c361dca6a1a8f2bc1a4
Shanmukh <96300319+DShanmukh@users.noreply.github.com>
2022-12-05 22:09:31 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,count=0;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int r,count1=0,tem;     for(i=0;i<n;i++)     {         tem=a[i];         count=0;         while(a[i]!=0)         {             r=a[i]%10;                count++;             a[i]=a[i]/10;         }         if(tem==0)         {             count+=1;         }         printf("%d ",count);
        }
    }

---
+++ count_digits.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,count=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int r,count1=0,tem;
+    for(i=0;i<n;i++)
+    {
+        tem=a[i];
+        count=0;
+        while(a[i]!=0)
+        {
+            r=a[i]%10;
+               count++;
+            a[i]=a[i]/10;
+        }
+        if(tem==0)
+        {
+            count+=1;
+        }
+        printf("%d ",count);
+    
+    }
+}
